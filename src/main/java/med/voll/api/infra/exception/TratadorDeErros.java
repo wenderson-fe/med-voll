f88ac1d22963924b6f5f9cda4459be74895bd3ab
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2025-04-10 04:04:34 -0300

    feat: tratamento para erros

--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -1,7 +1,11 @@
 package med.voll.api.infra.exception;
 
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,6 +27,26 @@ public class TratadorDeErros {
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
     }
 
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new Excecao("Credenciais inválidas"));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new Excecao("Falha na autenticação"));
+    }
+
+    @ExceptionHandler(JWTDecodeException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new Excecao("Acesso negado"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Excecao(ex.getLocalizedMessage()));
+    }
+
     private record DadosErroValidacao(String campo, String mensagem) {
         public DadosErroValidacao(FieldError error) {
             this(
@@ -31,4 +55,8 @@ public class TratadorDeErros {
             );
         }
     }
+
+    private record Excecao(String erro) {
+    }
+
 }
